263d8700937a1639b2fab84f4bc4f2c954c2bc9e
aminjamal <aminjamal10@gmail.com>
2020-07-16 01:57:57 +0430

    add ConverterInterface

---
+++ src/lib/Converter.py
@@ -0,0 +1,11 @@
+class ConverterInterface:
+    def convert(self):
+        pass
+
+
+class ToEPSG4326(ConverterInterface):
+    def __init__(self):
+        pass
+
+    def convert(self):
+        pass
