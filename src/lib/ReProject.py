d93159e9034e66113b49a6a111b8d793c0673b62
aminjamal <aminjamal10@gmail.com>
2020-07-16 02:12:39 +0430

    rename converter to reprojecter

---
+++ src/lib/ReProject.py
@@ -0,0 +1,11 @@
+class ReProjectInterface:
+    def convert(self):
+        pass
+
+
+class ToEPSG4326(ReProjectInterface):
+    def __init__(self):
+        pass
+
+    def re_project(self):
+        pass
