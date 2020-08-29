b1b6790a5aae6116168a3e4351919c2703fbcfdd
aminjamal <aminjamal10@gmail.com>
2020-08-29 14:26:54 +0430

    add raw_bucket name in __init__ of storage class

--- api/classes/Storage.py
+++ api/classes/Storage.py
@@ -1,7 +1,10 @@
+from api.settings import storage_settings
+
+
 class Storage:
     def __init__(self):
         self.raw: str
-        self._bucket_name: str
+        self._bucket_name: str = storage_settings.RAW_FILES_BUCKET
 
     def move(self):
         pass
