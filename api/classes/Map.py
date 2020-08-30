1d94868327009923860a56f7b2a71e690271ce2e
aminjamal <aminjamal10@gmail.com>
2020-08-30 13:46:27 +0430

    exeption handeling ValueError IndexError

--- api/classes/Map.py
+++ api/classes/Map.py
@@ -15,15 +15,24 @@ class Map:
         storage = StorageManagement(layer_bucket=storage_settings.LAYER_BUCKET)
         for layer in file_names:
             storage.apply_forced_path(layer.file_name)
-            result = (
-                await SingleLayer(
-                    request_model.coordinate.latitude,
-                    request_model.coordinate.longitude,
-                    storage.get_storage_path(),
+            try:
+                result = (
+                    await SingleLayer(
+                        request_model.coordinate.latitude,
+                        request_model.coordinate.longitude,
+                        storage.get_storage_path(),
+                    )
+                    .apply_s3(storage)
+                    .s3_extractor()
                 )
-                .apply_s3(storage)
-                .s3_extractor()
-            )
-            result.layer = None
-            results.append({"layer": layer.code, "result": result})
+                result.layer = None
+                results.append({"layer": layer.code, "result": result})
+            except (ValueError, IndexError) as ex:
+                error = ""
+                if isinstance(ex, IndexError):
+                    error = "out of range"
+                elif isinstance(ex, ValueError):
+                    error = "no-data"
+                results.append({"layer": layer.code, "result": error})
+
         return results
